d2954ba378d127caae6379becd70c221fb2d1481
yoobin <rladbdms0581@naver.com>
2017-11-02 13:05:37 +0900

    practice5 프로젝트 첫 commit

---
+++ src/practice5/Solution.java
@@ -0,0 +1,12 @@
+package practice5;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+	}
+
+}
